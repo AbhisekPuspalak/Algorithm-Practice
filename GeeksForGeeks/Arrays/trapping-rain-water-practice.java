41380af7a5288df94ed724f4ff93f758066630d6
AbhisekPuspalak <puspalakabhi@gmail.com>
2026-09-26 14:54:19 +0530

    Add GFG solution: Trapping Rain Water | Practice

---
+++ GeeksForGeeks/Arrays/trapping-rain-water-practice.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        // code here
+        int left=0;
+        int right=arr.length-1;
+        
+        int leftMax=0;
+        int rightMax=0;
+        
+        int totalWater=0;
+        
+        while(left<=right){
+            if(arr[left]<=arr[right]){
+                if(arr[left]>=leftMax){
+                    leftMax=arr[left];
+                }else{
+                    totalWater+=leftMax-arr[left];
+                }
+                left++;
+            }else{
+                if(arr[right]>=rightMax){
+                    rightMax=arr[right];
+                }else{
+                    totalWater+=rightMax-arr[right];
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
+}
